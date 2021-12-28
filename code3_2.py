73e48c175a00fbbac8fb40b463a64020ff09dc64
hogehoge <hogehoge@gmail.com>
2021-12-28 12:55:58 +0900

    if文 pythonでのif文

---
+++ code3_2.py
@@ -0,0 +1,7 @@
+name=input('名前＞＞')
+print('{}さん、こんにちは'.format(name))
+food=input('{}さんの好きな食べ物＞＞'.format(name))
+if food=='カレー':
+    print('素敵です、カレーは最高ですよね！！')
+else:
+    print('私も{}が好きですよ'.format(food))
